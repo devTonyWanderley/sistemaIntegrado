6c1d16114c04a101adc76274dd7bee4ed93305ee
devTonyWanderley <devtonywanderley@gmail.com>
2026-05-23 09:39:40 -0300

    âncora pra adotar string e abandonar a view

--- CPP/CORE/PROTOCOL/EquipoParser.hpp
+++ CPP/CORE/PROTOCOL/EquipoParser.hpp
@@ -121,6 +121,7 @@ Estrutura dos Campos:
 
 
 #pragma once
+#include <string>
 #include <string_view>
 #include <span>
 #include <cstdint>
@@ -132,28 +133,13 @@ struct RegistroDado
 
     constexpr RegistroDado(): dados{}{}
 
-    constexpr RegistroDado(
-        std::string_view id,
-        std::string_view atr,
-        std::string_view hi):
-        dados{id, atr, hi, "", "", ""}{}
+    constexpr RegistroDado(std::string_view id, std::string_view atr, std::string_view hi): dados{id, atr, hi, "", "", ""}{}
 
-    constexpr RegistroDado(
-        std::string_view id,
-        std::string_view atr,
-        std::string_view x,
-        std::string_view y,
-        std::string_view z):
+    constexpr RegistroDado(std::string_view id, std::string_view atr, std::string_view x, std::string_view y, std::string_view z):
         dados{id, atr, x, y, z, ""}{}
 
-    constexpr RegistroDado(
-        std::string_view id,
-        std::string_view atr,
-        std::string_view ah,
-        std::string_view av,
-        std::string_view di,
-        std::string_view hs):
-        dados{id, atr, ah, av, di, hs}{}
+    constexpr RegistroDado(std::string_view id, std::string_view atr, std::string_view ah, std::string_view av, std::string_view di,
+        std::string_view hs): dados{id, atr, ah, av, di, hs}{}
 
     constexpr std::string_view nome() const noexcept {return dados[0];}
     constexpr std::string_view atri() const noexcept {return dados[1];}
@@ -170,25 +156,147 @@ struct RegistroDado
 class ParserEquipo  //  classe base
 {
 public:
+    using cbReg = std::function<void(const RegistroDado&)>;
     virtual ~ParserEquipo() = default;
     [[nodiscard]] virtual std::string_view idEquipo() const noexcept = 0;
-    virtual uint64_t parseStream(std::string_view fBuffer, std::function<void(const RegistroDado&)> record_callback) = 0;
+    virtual uint64_t parse(std::string_view stream, cbReg rcb) = 0;
 };
 
 class ParserGD5 : public ParserEquipo
 {
 public:
-    [[nodiscard]] std::string_view idEquipo() const noexcept override {
-        return "Geodetic GD5 (.M21)";
+    constexpr ParserGD5() noexcept = default;
+    ~ParserGD5() override = default;
+
+    [[nodiscard]] std::string_view idEquipo() const noexcept override {return "GD5";}
+    uint64_t parse(std::string_view stream, cbReg rcb) override
+    {
+        if (stream.empty() || !rcb) return 0;
+        std::string bufferLimpo = higienizarStream(stream);
+        //-----------------------------------------------//
+        //-----------------------------------------------//
+        std::string_view sv(bufferLimpo);
+        uint64_t registrosProcessados = 0;
+        size_t indice = 0;
+        while (indice < sv.length())
+        {
+            indice = sv.find('_', indice);
+            if (indice == std::string_view::npos) break;
+            if (indice + 1 >= sv.length()) break;
+            char marcador = sv[indice + 1];
+            size_t proximoIndice = indice + 1;
+            if (marcador == '\'') proximoIndice = processarBlocoEstacao(sv, indice, rcb, registrosProcessados);
+            else if (marcador == '+') proximoIndice = processarBlocoPonto(sv, indice, rcb, registrosProcessados);
+            else proximoIndice = indice + 1;
+            indice = (proximoIndice > indice) ? proximoIndice : indice + 1;
+        }
+        return registrosProcessados;
+    }
+private:
+    std::string higienizarStream(std::string_view original)
+    {
+        std::string r = "";
+        r.reserve(original.size() * 128 / 136);
+        size_t pos = 1;
+        while(pos < original.size())
+        {
+            size_t n = (pos + 128 < original.size())? 128 : original.size() - pos;
+            r += original.substr(pos, n);
+            pos += 136;
+        }
+        std::string_view sv(r);
+        std::string res = "";
+        res.reserve(sv.length());
+        for(auto i = 0; i < sv.length(); i++)
+            if(sv[i] != ' ') res.push_back(sv[i]);
+        return res;
+    }
+    size_t processarBlocoEstacao(std::string_view stream, size_t pos, const cbReg& rcb, uint64_t& contador)
+    {
+        size_t fimBloco = stream.find('_', pos + 2);
+        if (fimBloco == std::string_view::npos) return stream.length();
+        std::string_view bloco = stream.substr(pos + 2, fimBloco - (pos + 2));
+        size_t abertAtrib = bloco.find("_(");
+        size_t fechAtrib = bloco.find("_)");
+        if (abertAtrib != std::string_view::npos && fechAtrib != std::string_view::npos)
+        {
+            std::string_view id = bloco.substr(0, abertAtrib);
+            std::string_view atrib = bloco.substr(abertAtrib + 2, fechAtrib - (abertAtrib + 2));
+            std::string_view hInst = bloco.substr(fechAtrib + 2);
+            rcb(RegistroDado(id, atrib, hInst));
+            contador++;
+        }
+        return fimBloco;
     }
 
-    uint64_t parseStream(std::string_view fBuffer, std::function<void(const RegistroDado&)> record_callback) override
+    size_t processarBlocoPonto(std::string_view stream, size_t pos, const cbReg& rcb, uint64_t& contador)
     {
-        uint64_t contador = 0;
-        if (fBuffer.empty()) return 0;
-        //  Processamento pra retornar M21
-        //record_callback(dado_teste);
-        contador++;
-        return contador;
+        size_t fimId = stream.find('_', pos + 2);
+        if (fimId == std::string_view::npos) return stream.length();
+        std::string_view id = stream.substr(pos + 2, fimId - (pos + 2));
+        size_t proximo = fimId + 1;
+        if (proximo >= stream.length()) return stream.length();
+        if (stream[proximo] == '<') return processarVisadaAngular(stream, proximo, id, rcb, contador);
+        if (stream[proximo] == '?') return processarMedicaoCompleta(stream, proximo, id, rcb, contador);
+        return proximo;
+    }
+
+    size_t processarVisadaAngular(std::string_view stream, size_t pos, std::string_view id, const cbReg& rcb, uint64_t& contador)
+    {
+        size_t fimAng = stream.find('>', pos);
+        if (fimAng == std::string_view::npos) return stream.length();
+        std::string_view blocoAng = stream.substr(pos + 1, fimAng - (pos + 1));
+        size_t primMais = blocoAng.find('+');
+        size_t segMais = (primMais != std::string_view::npos) ? blocoAng.find('+', primMais + 1) : std::string_view::npos;
+        if (primMais != std::string_view::npos && segMais != std::string_view::npos)
+        {
+            std::string_view av = blocoAng.substr(0, primMais);
+            std::string_view ah = blocoAng.substr(primMais + 1, segMais - (primMais + 1));
+            std::string_view hPri = buscarAlturaPrisma(stream, fimAng);
+            rcb(RegistroDado(id, "", ah, av, "", hPri));
+            contador++;
+        }
+        return fimAng;
+    }
+
+    size_t processarMedicaoCompleta(std::string_view stream, size_t pos, std::string_view id, const cbReg& rcb, uint64_t& contador)
+    {
+        size_t idxM = stream.find('m', pos);
+        size_t idxMais = stream.find('+', pos);
+        if (idxM == std::string_view::npos || idxMais == std::string_view::npos || idxMais > idxM) return pos + 1;
+        std::string_view dist = stream.substr(idxMais + 1, idxM - (idxMais + 1));
+        if (idxM + 1 + 7 <= stream.length() && idxM + 9 + 7 <= stream.length())
+        {
+            std::string_view av = stream.substr(idxM + 1, 7);
+            std::string_view ah = stream.substr(idxM + 9, 7);
+            std::string_view atrib = buscarAtributoOpcional(stream, idxM);
+            std::string_view hPri = buscarAlturaPrisma(stream, idxM);
+            rcb(RegistroDado(id, atrib, ah, av, dist, hPri));
+            contador++;
+        }
+        size_t proximoBloco = stream.find('_', idxM);
+        return (proximoBloco != std::string_view::npos) ? proximoBloco : stream.length();
+    }
+
+    std::string_view buscarAlturaPrisma(std::string_view stream, size_t posicaoBusca) const noexcept
+    {
+        size_t idxVirgula = stream.find(',', posicaoBusca);
+        if (idxVirgula != std::string_view::npos)
+        {
+            size_t fimPrisma = stream.find('_', idxVirgula);
+            if (fimPrisma != std::string_view::npos) return stream.substr(idxVirgula + 1, fimPrisma - (idxVirgula + 1));
+        }
+        return "";
+    }
+
+    std::string_view buscarAtributoOpcional(std::string_view stream, size_t posicaoBusca) const noexcept
+    {
+        size_t idxAtrib = stream.find("_*", posicaoBusca);
+        if (idxAtrib != std::string_view::npos)
+        {
+            size_t fimAtrib = stream.find('_', idxAtrib + 2);
+            if (fimAtrib != std::string_view::npos) return stream.substr(idxAtrib + 2, fimAtrib - (idxAtrib + 2));
+        }
+        return "";
     }
 };
--- CPP/PARSER/main.cpp
+++ CPP/PARSER/main.cpp
@@ -3,6 +3,133 @@
 #include <chrono>
 #include "EquipoParser.hpp"
 
-int main() {
+#include <fstream>
+#include <sstream>
+
+std::string ler()
+{
+    std::ifstream arquivo("C:\\DESENV\\CPP\\SERIAL-I\\build\\Desktop-Debug\\debug\\TMP\\20260517091828.tmp", std::ios::binary);
+    if(!arquivo.is_open()) return "";
+    std::stringstream r;
+    r << arquivo.rdbuf();
+    return r.str();
+}
+
+std::string lerLimpo()
+{
+    std::string original = ler();
+    if(original.empty()) return original;
+    std::string_view origem(original);
+    std::string limpo = "";
+    limpo.reserve(original.size() * (128 / 136));
+    size_t posi = 1;
+    while(posi < origem.size())
+    {
+        size_t n = (posi + 128 < origem.size())? 128 : origem.size() - posi;
+        limpo += origem.substr(posi, n);
+        posi += 136;
+    }
+    return limpo;
+}
+
+int main()
+{
+    std::string leituras = lerLimpo();
+    std::cout
+        << leituras.length()
+        << '\n'
+        << leituras.find("_\'", 0) + 2
+        << '\n'
+        << leituras.find("_+", 0)
+        << '\n'
+        << leituras.substr(2, 11)
+        << std::endl;
+    size_t pos = 0, posf;
+    while(true)
+    {
+        if(leituras.find("_\'", pos) != std::string::npos && leituras.find("_+", pos) != std::string::npos)
+        {
+            pos = (leituras.find("_\'", pos) < leituras.find("_+", pos))? leituras.find("_\'", pos) : leituras.find("_+", pos);
+            pos += 2;
+            if(leituras.find("_\'", pos) != std::string::npos && leituras.find("_+", pos) != std::string::npos)
+                posf = (leituras.find("_\'", pos) < leituras.find("_+", pos))? leituras.find("_\'", pos) : leituras.find("_+", pos);
+            else if(leituras.find("_\'", pos) != std::string::npos) posf = leituras.find("_\'", pos);
+            else if(leituras.find("_+", pos) != std::string::npos) posf = leituras.find("_+", pos);
+            else posf = leituras.length() - 1;
+        }
+        else if(leituras.find("_\'", pos) != std::string::npos)
+        {
+            pos = leituras.find("_\'", pos) + 2;
+            if(leituras.find("_\'", pos) != std::string::npos) posf = leituras.find("_\'", pos);
+            else posf = leituras.length() - 1;
+        }
+        else if(leituras.find("_+", pos) != std::string::npos)
+        {
+            pos = leituras.find("_+", pos) + 2;
+            if(leituras.find("_+", pos) != std::string::npos) posf = leituras.find("_+", pos);
+            else posf = leituras.length() - 1;
+        }
+        else break;
+        std::string linha = leituras.substr(pos, posf - pos);
+        if(linha.find("_(") != std::string::npos)
+        {
+            size_t i = 0, j = linha.find('_');
+            std::cout << linha.substr(i, j - i) << '\t';
+            i = linha.find('(') + 1;
+            j = linha.find('_', i);
+            std::cout << linha.substr(i, j - i) << '\t';
+            i = linha.find(')') + 1;
+            std::cout << linha.substr(i) << '\n';
+        }
+        //std::cout << leituras.substr(pos, posf - pos) << '\n';
+        else if(linha.find('<') != std::string::npos)
+        {
+            //1_ <0811413+1194753+****d090_*P_,1.600_
+            //1_
+            size_t i = 0, j = linha.find('_');
+            std::cout << linha.substr(i, j - i) << '\t';
+            //<0811413+
+            i = linha.find('<') + 1;
+            j = linha.find('+', i);
+            std::cout << linha.substr(i, j - i) << '\t';
+            //+1194753+
+            i = linha.find('+') + 1;
+            j = linha.find('+', i);
+            std::cout << linha.substr(i, j - i) << '\t';
+            //_*P_,
+            i = linha.find("_*") + 2;
+            j = linha.find("_,", i);
+            std::cout << linha.substr(i, j - i) << '\t';
+            i = linha.find("_,") + 2;
+            std::cout << linha.substr(i) << '\n';
+        }
+        else
+        {
+            //1_ ?+00023023m0811410+1194753d+00022754***+25-30050_*P_,1.600
+            //1_
+            size_t i = 0, j = linha.find('_');
+            std::cout << linha.substr(i, j - i) << '\t';
+            //?+00023023m
+            i = linha.find("?+") + 2;
+            j = linha.find('m', i);
+            std::cout << linha.substr(i, j - i) << '\t';
+            //m0811410+
+            i = linha.find('m') + 1;
+            j = linha.find('+', i);
+            std::cout << linha.substr(i, j - i) << '\t';
+            //+1194753d+
+            i = linha.find('+', i) + 1;
+            j = linha.find("d+", i);
+            std::cout << linha.substr(i, j - i) << '\t';
+            //_*P_
+            i = linha.find("_*", i) + 2;
+            j = linha.find('_', i);
+            std::cout << linha.substr(i, j - i) << '\t';
+            //_,1.600
+            i = linha.find("_,", i) + 2;
+            std::cout << linha.substr(i) << '\n';
+        }
+        //else std::cout << linha << '\n';
+    }
     return 0;
 }
